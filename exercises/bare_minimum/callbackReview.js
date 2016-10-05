1d8cafb43800f59ef62bf2d5d51d4ba4abbcb32c
Hans Trautlein <hans@trautlein.com>
2016-10-05 12:00:46 -0700

    Finally this commit went through...

--- exercises/bare_minimum/callbackReview.js
+++ exercises/bare_minimum/callbackReview.js
@@ -5,14 +5,28 @@
 var fs = require('fs');
 var request = require('request');
 
+
 // This function should retrieve the first line of the file at `filePath`
-var pluckFirstLineFromFile = function (filePath) {
-  // TODO
+var pluckFirstLineFromFile = function (filePath, callback) {
+
+  fs.readFile(filePath, (err, data) => {
+    if (err) { callback(err, null); }
+    var firstL = data.toString().split('\n')[0];
+    callback(null, firstL);
+  });
+
+  // data.toString().split('\n')[0];
 };
 
 // This function should retrieve the status code of a GET request to `url`
 var getStatusCode = function (url, callback) {
-  // TODO
+  request(url, function (err, response) {
+    if (err) { 
+      callback(err, null);
+    } else {
+      callback(null, response.statusCode);
+    }
+  });
 };
 
 // Export these functions so we can test them and reuse them in later exercises
